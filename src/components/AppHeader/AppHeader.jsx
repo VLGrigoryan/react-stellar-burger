e7fb57e694e902e8182ecae28441cbd150a6cc3b
VLGrigoryan <v.grigoryan@live.com>
2023-06-25 01:00:01 +0200

    App-header

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -9,51 +9,42 @@ import HeaderStyle from "./AppHeader.module.css";
 
 const AppHeader = () => {
   return (
-    <header className={HeaderStyle.header + " pt-4 pb-4"}>
+    <header className={`${HeaderStyle.header} pt-4 pb-4`}>
       <nav className={HeaderStyle.nav}>
-        <ul className={HeaderStyle.list} style={{ marginRight: 112 }}>
-          <li className={HeaderStyle.item + " pt-4 pb-4 pr-5 pl-5 mr-2"}>
+        <div className={`${HeaderStyle.list} mr-28`}>
+          <a
+            href="#"
+            className={`${HeaderStyle.link} ${HeaderStyle.primaryLink} pt-4 pb-4 pr-5 pl-5 mr-2`}
+          >
             <BurgerIcon type="primary" />
-            <a
-              href="#"
-              className={
-                HeaderStyle.none +
-                " text text_color_primary text_type_main-default ml-2"
-              }
-            >
+            <span className="text text_color_primary text_type_main-default ml-2">
               Конструктор
-            </a>
-          </li>
-          <li className={HeaderStyle.item + " pt-4 pb-4 pr-5 pl-5"}>
+            </span>
+          </a>
+          <a
+            href="#"
+            className={`${HeaderStyle.link} ${HeaderStyle.secondaryLink} pt-4 pb-4 pr-5 pl-5`}
+          >
             <ListIcon type="secondary" />
-            <a
-              href="#"
-              className={
-                HeaderStyle.none +
-                " text text_color_inactive text_type_main-default ml-2"
-              }
-            >
+            <span className="text text_color_inactive text_type_main-default ml-2">
               Лента заказов
-            </a>
-          </li>
-        </ul>
+            </span>
+          </a>
+        </div>
         <div className={HeaderStyle.logo}>
           <Logo />
         </div>
-        <ul className={HeaderStyle.list} style={{ marginLeft: "auto" }}>
-          <li className={HeaderStyle.item + " pt-4 pb-4 pr-5 pl-5"}>
+        <div className={`${HeaderStyle.list} ${HeaderStyle.rightList}`}>
+          <a
+            href="#"
+            className={`${HeaderStyle.link} ${HeaderStyle.secondaryLink} pt-4 pb-4 pr-5 pl-5`}
+          >
             <ProfileIcon type="secondary" />
-            <a
-              href="#"
-              className={
-                HeaderStyle.none +
-                " text text_color_inactive text_type_main-default ml-2"
-              }
-            >
+            <span className="text text_color_inactive text_type_main-default ml-2">
               Личный кабинет
-            </a>
-          </li>
-        </ul>
+            </span>
+          </a>
+        </div>
       </nav>
     </header>
   );
