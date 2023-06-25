6b789766fadc8ec6008233fc97e7dc80c121ca35
VLGrigoryan <v.grigoryan@live.com>
2023-06-25 03:35:59 +0200

    last-corrections

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
-import {ingredientPropType} from '../../utils/prop-types';
 import CStyle from './Card.module.css';
+import {ingredientPropType} from '../../utils/prop-types';
 
 const Card = (props) => {
   const [count, setCount] = useState(props.count || 0);
--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -1,10 +1,10 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-export const ingredientPropType = PropTypes.shape({
+export const ingredientPropType = {
   count: PropTypes.number,
-  _id: PropTypes.string.isRequired,
+  _id: PropTypes.string,
   image: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   onClick: PropTypes.func,
-  });
+};
