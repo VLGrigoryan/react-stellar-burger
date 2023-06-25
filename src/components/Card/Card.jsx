fa95ff8462cdd806b4283570da31249d3cc4e46d
VLGrigoryan <v.grigoryan@live.com>
2023-06-25 09:53:16 +0200

    card-fix

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import CStyle from './Card.module.css';
 import { ingredientPropType } from '../../utils/prop-types';
 
 const Card = (props) => {
-  const [count, setCount] = useState(props.count || 0);
-
   const cardKey = `${props._id}-${props.count || 0}`;
 
   return (
     <li className={CStyle.card} key={cardKey}>
       <div className={`${CStyle['card-container']} pl-4 pr-4`} onClick={props.onClick}>
         <img src={props.image} alt={props.name} />
-        {count > 0 && <Counter count={count} size="default" />}
+        {props.count > 0 && <Counter count={props.count} size="default" />}
       </div>
       <div style={{ display: 'flex' }} className="mt-1 mb-1">
         <p className="text text_type_digits-default mr-2">{props.price}</p>
